21701b43fd4be82ad067cdb9ce24723228cc9108
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2023-09-30 18:07:26 +0100

    Test files and package.json updated

--- src/ninjas/ninjas.controller.spec.ts
+++ src/ninjas/ninjas.controller.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NinjasController } from './ninjas.controller';
+import { NinjasService } from './ninjas.service';
 
 describe('NinjasController', () => {
   let controller: NinjasController;
+  let service: NinjasService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NinjasController],
+      providers: [NinjasService],
     }).compile();
 
     controller = module.get<NinjasController>(NinjasController);
+    service = module.get<NinjasService>(NinjasService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
